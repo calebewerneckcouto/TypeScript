e8993a6c083de1684b04d263ad892889591235a9
calebewerneckcouto <calebewerneck@hotmail.com>
2024-06-27 18:54:07 -0300

    instalando typescript

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-let n1 = document.getElementById("n1");
-let n2 = document.getElementById("n2");
-let botao = document.getElementById("calcular");
-let res = document.getElementById("resultado");
-
-
-function calcular (n1,n2){
-
-    if(typeof n1 === 'number' && typeof n2 ==='number'){
+var n1 = document.getElementById("n1");
+var n2 = document.getElementById("n2");
+var botao = document.getElementById("calcular");
+var res = document.getElementById("resultado");
+function calcular(n1, n2) {
+    if (typeof n1 === 'number' && typeof n2 === 'number') {
         return n1 + n2;
-    }else{
+    }
+    else {
         n1 = parseInt(n1);
         n2 = parseInt(n2);
-
         return n1 + n2;
-
     }
-
-    
-
 }
-
-
-botao.addEventListener('click',function () {
-    res.innerHTML = calcular(n1.value,n2.value);
-})
+botao.addEventListener('click', function () {
+    res.innerHTML = calcular(n1.value, n2.value);
+});
